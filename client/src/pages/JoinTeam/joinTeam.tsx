d5b607728eab126a61174f5d2c7e5af7a153591d
yashudkl <yashudkl@gmail.com>
2024-12-05 13:52:38 +0545

    main branch

--- client/src/pages/JoinTeam/joinTeam.tsx
+++ client/src/pages/JoinTeam/joinTeam.tsx
@@ -10,7 +10,7 @@ const joinTeam: React.FC = () => {
           Join A Team 
         </h1>
         <p>Randomly join a team based on your preferences</p>
-        <div className='space-y-2'>
+        <div className='space-y-5'>
         <p className="text-[16px] font-bold mt-4 text-[#384B6B] font-poppins">
           Enter your category
         </p>
